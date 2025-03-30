1528644a96afa599372997345a4f203f435bbc31
rowidAAA <rowida.abdelghany05@eng-st.cu.edu.eg>
2025-03-30 14:57:40 +0200

    k

--- DS_Physiotherapy_Center/Treatment.h
+++ DS_Physiotherapy_Center/Treatment.h
@@ -1,8 +1,19 @@
 #pragma once
+#include"Patient.h"
 class Treatment
 {
 	int duration;
 	int assignmentTime;
+	Patient* Patient;
+	// pointer to assigned resource lma roaa t3mlo
+	// Resource* AssignedResource;
+public:
+	Treatment(int d=0, int AT=0):Patient(nullptr) {
+		duration = d;
+		assignmentTime = AT;
+	}
+	virtual bool CanAssign();
+	virtual void MoveToWait();
 
 };
 
